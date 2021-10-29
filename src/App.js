0718fd1abdd1da574f827831cc36ae86bdcc02fd
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-10-29 15:12:52 +0200

    Remove card component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MoreHoriz from "@mui/icons-material/MoreHoriz";
+import ShareIcon from "@mui/icons-material/Share";
 import React from "react";
 import "./App.css";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreHoriz from "@mui/icons-material/MoreHoriz";
-import { Icons } from "./Icons/Icons";
 import data from "./Data/MOCK_DATA.json";
+import { Icons } from "./Icons/Icons";
 
 function App() {
   return (
@@ -48,32 +37,18 @@ function App() {
               <div>
                 <img className="post-img" src={datum.post_image} alt="" />
               </div>
-
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  {datum.is_post_liked ? (
-                    <FavoriteIcon style={{ color: "red" }} />
-                  ) : (
-                    <FavoriteIcon />
-                  )}
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-              </CardActions>
-              <CardContent>
-                <Typography
-                  variant="body2"
-                  color="text.primary"
-                  sx={{ width: 614, maxHeight: 17.6 }}
-                  style={{ marginBottom: "2%" }}
-                >
-                  {datum.likes_count} likes
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {datum.post_caption}
-                </Typography>
-              </CardContent>
+              <div disableSpacing>
+                {datum.is_post_liked ? (
+                  <FavoriteIcon style={{ color: "red", margin: "0 2%" }} />
+                ) : (
+                  <FavoriteIcon style={{ margin: "0 2%" }} />
+                )}
+                <ShareIcon />
+              </div>
+              <div className="post-caption-section">
+                <p className="like-count">{datum.likes_count} likes</p>
+                <p>{datum.post_caption}</p>
+              </div>
             </div>
           </div>
         </div>
