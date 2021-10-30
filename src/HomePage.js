1e34609bd5ccdd2de9831fb5e888038a0de6481d
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-10-30 18:39:27 +0200

    Disable scroll when popup is active , clean css

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,17 +3,29 @@ import React from "react";
 import "./App.css";
 import data from "./Data/MOCK_DATA.json";
 import { NavBar } from "./NavBar/NavBar";
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export function HomePage() {
   const [backgroundColor, setBackgroundColor] = React.useState(false);
+  backgroundColor ? disableBodyScroll(document) : enableBodyScroll(document)
   return (
-    <div style={ backgroundColor ? {backgroundColor: "rgba(0, 0, 0, 0.8)"} : {backgroundColor: ""}} className="content-section">
+    <div
+      style={
+        backgroundColor
+          ? { backgroundColor: "rgba(0, 0, 0, 0.8)" }
+          : { backgroundColor: "" }
+      }
+      className="content-section"
+    >
       <NavBar backgroundColor={backgroundColor} />
       {data.map((datum) => (
-        <Post datum={datum} backgroundColor={backgroundColor} comments={datum.comments} setBackgroundColor={setBackgroundColor} />
+        <Post
+          datum={datum}
+          backgroundColor={backgroundColor}
+          comments={datum.comments}
+          setBackgroundColor={setBackgroundColor}
+        />
       ))}
     </div>
   );
 }
-
-
