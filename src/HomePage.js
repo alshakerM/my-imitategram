cab3a3be6c797d12c85148a27a333e49f21191ae
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-10-30 18:25:36 +0200

    Clean css

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,7 +7,7 @@ import { NavBar } from "./NavBar/NavBar";
 export function HomePage() {
   const [backgroundColor, setBackgroundColor] = React.useState(false);
   return (
-    <div style={backgroundColor ? {backgroundColor: "rgba(0, 0, 0, 0.8)"} : {backgroundColor: ""}} className="content-section">
+    <div style={ backgroundColor ? {backgroundColor: "rgba(0, 0, 0, 0.8)"} : {backgroundColor: ""}} className="content-section">
       <NavBar backgroundColor={backgroundColor} />
       {data.map((datum) => (
         <Post datum={datum} backgroundColor={backgroundColor} comments={datum.comments} setBackgroundColor={setBackgroundColor} />
