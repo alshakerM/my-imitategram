755ee597c80febef77ffc90b0e5d80fa45ca1d85
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-11-03 20:10:48 +0100

    Add like icon to comments

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -11,34 +11,19 @@ function PostActions(is_post_liked) {
   return (
     <div className="post-actions">
       <div className="like-share-telegram-icons">
-        {is_post_liked ? (
-          <svg
-            aria-label="Unlike"
-            class="_8-yf5 "
-            color="#ed4956"
-            fill="#ed4956"
-            height="24"
-            role="img"
-            viewBox="0 0 48 48"
-            width="24"
-          >
-            <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-          </svg>
-        ) : (
-          <svg
-            aria-label="Like"
-            className="Like-icon"
-            style={{ color: "red" }}
-            color="#8e8e8e"
-            fill="#8e8e8e"
-            height="24"
-            role="img"
-            viewBox="0 0 48 48"
-            width="24"
-          >
-            <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-          </svg>
-        )}
+        <svg
+          aria-label="Unlike"
+          class="_8-yf5 "
+          color={is_post_liked ? "#ed4956" : "#8e8e8e"}
+          fill={is_post_liked ? "#ed4956" : "#8e8e8e"}
+          height="24"
+          role="img"
+          viewBox="0 0 48 48"
+          width="24"
+        >
+          <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+        </svg>
+
         <svg
           aria-label="Comment"
           className="comment-icon"
@@ -166,14 +151,54 @@ export function Post({ datum, isExtended, setIsExtended, index, isFloating }) {
           View all comments{" "}
         </Link>
         <div className="comment-section">
-          <p className="comment-info">
-            <strong>{datum.comments[0].user_name}</strong>{" "}
-            {datum.comments[0].comment}
-          </p>
-          <p className="comment-info">
-            <strong>{datum.comments[1].user_name}</strong>{" "}
-            {datum.comments[1].comment}
-          </p>
+          <div className="comment-info">
+            <p className="comment-username-comment-text">
+              <strong>{datum.comments[0].user_name}</strong>{" "}
+              {datum.comments[0].comment}
+            </p>
+            <p>
+              <svg
+                aria-label="Like"
+                class="_8-yf5 "
+                color={
+                  datum.comments[0].is_liked_by_user ? "#ed4956" : "#8e8e8e"
+                }
+                fill={
+                  datum.comments[0].is_liked_by_user ? "#ed4956" : "#8e8e8e"
+                }
+                height="12"
+                role="img"
+                viewBox="0 0 48 48"
+                width="12"
+              >
+                <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+              </svg>
+            </p>
+          </div>
+          <div className="comment-info">
+            <p className="comment-username-comment-text">
+              <strong>{datum.comments[1].user_name}</strong>{" "}
+              {datum.comments[1].comment}
+            </p>
+            <p>
+              <svg
+                aria-label="Like"
+                class="_8-yf5 "
+                color={
+                  datum.comments[1].is_liked_by_user ? "#ed4956" : "#8e8e8e"
+                }
+                fill={
+                  datum.comments[1].is_liked_by_user ? "#ed4956" : "#8e8e8e"
+                }
+                height="12"
+                role="img"
+                viewBox="0 0 48 48"
+                width="12"
+              >
+                <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+              </svg>
+            </p>
+          </div>
           <PostDate posting_time={datum.posting_time} isExtended={isExtended} />
         </div>{" "}
       </div>
