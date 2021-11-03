3da4799a3d3647816093c00571c3806e18ca21ac
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-11-03 15:30:08 +0100

    Add save icon

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -8,6 +8,7 @@ import classnames from "classnames";
 function PostActions(is_post_liked) {
   return (
     <div className="post-actions">
+      <div className="like-share-telegram-icons">
       {is_post_liked ? (
         <svg
           aria-label="Unlike"
@@ -63,6 +64,19 @@ function PostActions(is_post_liked) {
         width="22"
       >
         <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
+      </svg>
+      </div>
+      <svg
+        aria-label="Save"
+        className="save-icon"
+        color="#8e8e8e"
+        fill="#8e8e8e"
+        height="24"
+        role="img"
+        viewBox="0 0 48 48"
+        width="24"
+      >
+        <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z"></path>
       </svg>
     </div>
   );
