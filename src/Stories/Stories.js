ead9c07120fb0cd377eef7902649367de2351992
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-11-18 03:07:03 +0100

    Fix stories scroll buttons

--- src/Stories/Stories.js
+++ src/Stories/Stories.js
@@ -1,28 +1,29 @@
-import "./Stories.css";
-import React, { useState } from "react";
-import { StoryAvatar } from "../StoryAvatar/StoryAvatar";
-import ChevronLeft from "@mui/icons-material/ChevronLeft";
-import ChevronRight from "@mui/icons-material/ChevronRight";
-import classnames from "classnames";
+import './Stories.css';
+import React, { useState } from 'react';
+import { StoryAvatar } from '../StoryAvatar/StoryAvatar';
+import ChevronLeft from '@mui/icons-material/ChevronLeft';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import classnames from 'classnames';
 
 function width(el) {
   return el?.getBoundingClientRect().width;
 }
 
-
 export function Stories() {
   const allCirclesDiv = React.useRef();
-  const allCirclesWidth = 449;//width(allCirclesDiv.current);
+  const allCirclesWidth = width(allCirclesDiv.current);
   const [containerWidth, setContainerWidth] = useState(0);
   const [scrollLeft, setScrollLeft] = React.useState(0);
   const scrollLimit = -1 * (allCirclesWidth - containerWidth);
-  const [storiesData, setStoriesData] = React.useState([])
+  const [storiesData, setStoriesData] = React.useState([]);
   React.useEffect(() => {
-    fetch("../Data/IG-Stories.json" , {
-      method: "GET"
-    }).then((res) => res.json())
-    .then((results) => setStoriesData(results))
+    fetch('../Data/IG-Stories.json', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((results) => setStoriesData(results));
   }, []);
+  console.log({ scrollLeft, scrollLimit, allCirclesWidth, containerWidth });
   return (
     <div className="stories-avatars-container">
       <div
@@ -38,31 +39,28 @@ export function Stories() {
             <StoryAvatar user={user} index={index} isUserNameNeeded={true} />
           ))}
         </div>
-        
-          <button
-            onClick={() => {
-              setScrollLeft((s) => Math.min(0, s + containerWidth / 2));
-            }}
-            className={classnames("story-nav-prev", {
-              hidden: scrollLeft === 0,
-            })}
-          >
-            <ChevronLeft />
-          </button>
 
-          <button
-            onClick={() => {
-              setScrollLeft((s) =>
-                Math.max(scrollLimit, s - containerWidth / 2)
-              );
-            }}
-            className={classnames("story-nav-next", {
-              hidden: scrollLeft <= scrollLimit,
-            })}
-          >
-            <ChevronRight />
-          </button>
-        
+        <button
+          onClick={() => {
+            setScrollLeft((s) => Math.min(0, s + containerWidth / 2));
+          }}
+          className={classnames('story-nav-prev', {
+            hidden: scrollLeft === 0,
+          })}
+        >
+          <ChevronLeft />
+        </button>
+
+        <button
+          onClick={() => {
+            setScrollLeft((s) => Math.max(scrollLimit, s - containerWidth / 2));
+          }}
+          className={classnames('story-nav-next', {
+            hidden: scrollLeft === scrollLimit,
+          })}
+        >
+          <ChevronRight />
+        </button>
       </div>
     </div>
   );
