e32fb5db93f9e782ff02daf344e48b74e5a996de
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-11-24 15:20:27 +0100

    Change if statement in icons

--- src/Icons/Icons.js
+++ src/Icons/Icons.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import './Icons.css';
 
-export function Icons({from_user_id}) {
+export function Icons({ from_user_id }) {
   const location = useLocation();
   return (
     <div className="icons-container">
@@ -36,7 +36,7 @@ export function Icons({from_user_id}) {
           viewBox="0 0 48 48"
           width="22"
         >
-          {location.pathname === '/direct/inbox' || location.pathname === `/direct/t/${from_user_id}`? (
+          {location.pathname.startsWith('/direct/') ? (
             <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l13.2 13c.5.4 1.1.6 1.7.3l16.6-8c.7-.3 1.6-.1 2 .5.4.7.2 1.6-.5 2l-15.6 9.9c-.5.3-.8 1-.7 1.6l4.6 19c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.5-.5.5-1.1.2-1.6z"></path>
           ) : (
             <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
