1b1a348e443005c2092a343a5ca183b0b60f4ebe
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-12-07 14:08:22 +0100

    Change the animation of ColorRing

--- src/Avatar/Avatar.js
+++ src/Avatar/Avatar.js
@@ -15,21 +15,15 @@ export function Avatar({
   switch (size) {
     case 'small':
       dimensions.SVGDiameter = 42;
-      dimensions.imgDiameter = 32;
-      dimensions.strokeWidth = 2;
-      dimensions.imgMargin = 5;
+      dimensions.strokeWidth = 10;
       break;
     case 'medium':
       dimensions.SVGDiameter = 66;
-      dimensions.imgDiameter = 56;
-      dimensions.strokeWidth = 2;
-      dimensions.imgMargin = 5;
+      dimensions.strokeWidth = 8;
       break;
     case 'large':
       dimensions.SVGDiameter = 168;
-      dimensions.imgDiameter = 150;
-      dimensions.strokeWidth = 3;
-      dimensions.imgMargin = 9;
+      dimensions.strokeWidth = 5;
       break;
     default:
       dimensions.imgDiameter = size;
@@ -40,37 +34,34 @@ export function Avatar({
   return (
     <Link to={`/stories/${user.user_id}`} className={className}>
       <svg
-        viewBox={`0 0 ${dimensions.SVGDiameter} ${dimensions.SVGDiameter}`}
+        viewBox={`0 0 300 300`}
         width={dimensions.SVGDiameter}
         className={styles.animationSvg}
       >
         {colorRing && (
-          <circle
-            className={cx(styles.animationCircle, {
-              [styles.isAnimated]: animate,
-            })}
-            cx={dimensions.SVGDiameter / 2}
-            cy={dimensions.SVGDiameter / 2}
-            r={dimensions.SVGDiameter / 2 - dimensions.strokeWidth}
-            stroke={isUnread ? 'url(#ig-grad)' : '#8e8e8e'}
-            stroke-width={dimensions.strokeWidth}
-            fill="none"
-            stroke-linecap="round"
-          />
+          <g>
+            <circle
+              className={cx(styles.animationCircle, {
+                [styles.isAnimated]: animate,
+              })}
+              cx={150}
+              cy={150}
+              r={140}
+              stroke={isUnread ? 'url(#ig-grad)' : '#8e8e8e'}
+              stroke-width={dimensions.strokeWidth}
+              fill="none"
+              stroke-linecap="round"
+            />
+          </g>
         )}
-        <foreignObject
-          x="0"
-          y="0"
-          width={dimensions.SVGDiameter}
-          height={dimensions.SVGDiameter}
-        >
+        <foreignObject x="0" y="0" width="300" height="300">
           <img
-            style={{ margin: dimensions.imgMargin }}
+            style={{ margin: 20 }}
             src={user?.user_thumbnail}
             alt={`${user?.user_name} avatar`}
             className={styles.userAvatar}
-            width={dimensions.imgDiameter}
-            height={dimensions.imgDiameter}
+            width="260"
+            height="260"
           />
         </foreignObject>
       </svg>
