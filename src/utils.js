db578c097050fac91d0acb940354b1353186daa1
alshakerM <82781870+alshakerM@users.noreply.github.com>
2021-12-07 18:04:18 +0100

    Change counts to digitGrouping

--- src/utils.js
+++ src/utils.js
@@ -32,18 +32,17 @@ export function lockBodyScrolls(lock) {
  * @returns returns the the number as human readable number
  */
 export function readableNumber(num) {
-  if (num > 10000000) {
-    return `${(num / 10000000).toFixed(1)}m`;
-  }
   if (num > 1000000) {
     return `${(num / 1000000).toFixed(1)}m`;
   } else if (num > 100000) {
     return `${Math.floor(num / 1000)}k`;
-  } else if (num > 10000) {
+  } else if (num >= 10000) {
     return `${(num / 1000).toFixed(1)}k`;
-  } else if (num > 1000) {
-    return `${(num / 100).toFixed(2)}`;
-  } else if (num < 1000) {
-    return num;
+  } else if (num < 10000) {
+    return digitGrouping(num);
   }
+  return num;
+}
+export function digitGrouping(num) {
+  return Intl.NumberFormat('en-us', { useGrouping: true }).format(num);
 }
