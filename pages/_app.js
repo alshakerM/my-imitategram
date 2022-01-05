46fc8f54820362f9e26277eadda715f67d3ee6a0
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-05 16:55:44 +0100

    Add dynamic-routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
+const routeTitleMap = {
+  '/direct/t/[userId]': 'Inbox • Direct',
+  '/direct/inbox': 'Inbox • Direct',
+  '/stories/[userId]': 'Stories • Instagram',
+};
+
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
   return (
     <div>
       <Head>
-        <title>Instagram Clone</title>
+        <title>{routeTitleMap[pathname] || 'Instagram Clone'}</title>
         <link rel="shortcut icon" href="/Instagarm logo.png" />
       </Head>
       <Component {...pageProps} />
