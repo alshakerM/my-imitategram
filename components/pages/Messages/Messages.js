c2d7b805da94f881441f78367a146babc59010a6
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-05 03:25:03 +0100

    Fix onBlur bug

--- components/pages/Messages/Messages.js
+++ components/pages/Messages/Messages.js
@@ -111,7 +111,17 @@ export function Messages({ fromUserId }) {
   const [isDotsClicked, setIsDotsClicked] = React.useState(false);
   return (
     <>
-      <div className={styles.messagesPageContainer}>
+      <div
+        className={styles.messagesPageContainer}
+        onClick={(event) => {
+          // hide the context-menu when you click outside of it
+          if (
+            !event.target.matches(`.${styles.messageTextAndThreeDots} *`)
+          ) {
+            setMessageIndex(undefined);
+          }
+        }}
+      >
         <div className={styles.leftSection}>
           <UserSection />
           <SenderSection fromUserId={fromUserId} />
@@ -174,7 +184,6 @@ export function Messages({ fromUserId }) {
                       className={cx(styles.messagesUserThumbnailMessageText, {
                         [styles.isSent]: message.direction === 'sent',
                       })}
-                      onBlur={() => setMessageIndex(undefined)}
                     >
                       <div className={styles.usersMessagesText}>
                         {message.direction !==
@@ -221,6 +230,7 @@ export function Messages({ fromUserId }) {
                                     className={styles.interactButtons}
                                     onClick={() => {
                                       deleteMessage(fromUserId, index);
+                                      setMessageIndex(undefined);
                                     }}
                                   >
                                     Unsend
@@ -366,18 +376,7 @@ export function Messages({ fromUserId }) {
                             [styles.isSent]: message.direction === 'sent',
                           })}
                         >
-                          <svg
-                            aria-label="like-icon"
-                            height="14"
-                            role="img"
-                            viewBox="0 0 48 48"
-                            width="14"
-                          >
-                            <path
-                              className={styles.likeIcon}
-                              d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"
-                            ></path>
-                          </svg>
+                          ❤️
                         </div>
                       )}
                     </div>
