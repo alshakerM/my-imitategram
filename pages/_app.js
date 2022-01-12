bcdaffe14d5d361fe035428bb4c7bea6dde98ef1
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-12 01:12:54 +0100

    Added colored loading bar to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import React from 'react';
 import '../styles/globals.css';
 
 const routeTitleMap = {
@@ -9,13 +10,32 @@ const routeTitleMap = {
 };
 
 function MyApp({ Component, pageProps }) {
-  const { pathname } = useRouter();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState(false);
+  React.useEffect(() => {
+    const handleStart = () => {
+      setIsLoading(true);
+    };
+    const handleEnd = () => {
+      setIsLoading(false);
+    };
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routerChangeComplete', handleEnd);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routerChangeComplete', handleEnd);
+    };
+  }, [router]);
+  console.log(isLoading);
   return (
     <div>
       <Head>
-        <title>{routeTitleMap[pathname] || 'Instagram Clone'}</title>
+        <title>{routeTitleMap[router.pathname] || 'Instagram Clone'}</title>
         <link rel="shortcut icon" href="/Instagarm logo.png" />
       </Head>
+      {isLoading && <div className="loading-bar"></div>}
+
       <Component {...pageProps} />
     </div>
   );
