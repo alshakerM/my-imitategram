8f3fced994d6a0e83f6c1b8b4496522167feb4e4
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-13 18:43:07 +0100

    Add mobile navbar

---
+++ components/Icons/MobileIcons.js
@@ -0,0 +1,191 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Icons.module.css';
+import { useRouter } from 'next/router';
+
+export function TopMobileIcons({ className }) {
+  const router = useRouter();
+  const pageURL = router.asPath;
+  return (
+    <div className={className}>
+      <svg
+        aria-label="New story"
+        color="#262626"
+        fill="#262626"
+        height="24"
+        role="img"
+        viewBox="0 0 24 24"
+        width="24"
+      >
+        <circle
+          cx="12"
+          cy="13.191"
+          fill="none"
+          r="4.539"
+          stroke="currentColor"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        ></circle>
+        <path
+          d="M18.592 21.374A3.408 3.408 0 0022 17.966V8.874a3.41 3.41 0 00-3.41-3.409h-.52a2.108 2.108 0 01-1.954-1.375 2.082 2.082 0 00-2.204-1.348h-3.824A2.082 2.082 0 007.884 4.09 2.108 2.108 0 015.93 5.465h-.52A3.41 3.41 0 002 8.875v9.091a3.408 3.408 0 003.408 3.408z"
+          fill="none"
+          stroke="currentColor"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        ></path>
+      </svg>
+      <Link href="/">
+        <a className={styles.navTitle}>
+          <Image
+            src="/Instagram_logo_text.svg"
+            width={840}
+            height={300}
+            layout="responsive"
+            alt="Instagram text"
+            className={styles.titleImg}
+          />
+        </a>
+      </Link>
+      <Link href="/direct/inbox">
+        <a>
+          <svg
+            aria-label="Direct"
+            className={styles.icon}
+            color="#262626"
+            fill="#262626"
+            height="22"
+            role="img"
+            viewBox="0 0 48 48"
+            width="22"
+          >
+            {pageURL.startsWith('/direct') ? (
+              <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l13.2 13c.5.4 1.1.6 1.7.3l16.6-8c.7-.3 1.6-.1 2 .5.4.7.2 1.6-.5 2l-15.6 9.9c-.5.3-.8 1-.7 1.6l4.6 19c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.5-.5.5-1.1.2-1.6z"></path>
+            ) : (
+              <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
+            )}
+          </svg>
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+export function BottomMobileIcons({ className }) {
+  const router = useRouter();
+  const pageURL = router.asPath;
+
+  return (
+    <div className={className}>
+      <Link href="/">
+        <a>
+          <svg
+            aria-label="Home"
+            className={styles.homeIcon}
+            color="#262626"
+            fill="#262626"
+            height="22"
+            role="img"
+            viewBox="0 0 48 48"
+            width="22"
+          >
+            {pageURL === '/' ? (
+              <path d="M45.5 48H30.1c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2.1-4.6-4.6-4.6s-4.6 2.1-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.6-.6 2.1 0l21.5 21.5c.3.3.4.7.4 1.1v23.5c.1.8-.6 1.5-1.4 1.5z"></path>
+            ) : (
+              <path d="M45.3 48H30c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2-4.6-4.6-4.6s-4.6 2-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.5-.6 2.1 0l21.5 21.5c.4.4.6 1.1.3 1.6 0 .1-.1.1-.1.2v22.8c.1.8-.6 1.5-1.4 1.5zm-13.8-3h12.3V23.4L24 3.6l-20 20V45h12.3V34.2c0-4.3 3.3-7.6 7.6-7.6s7.6 3.3 7.6 7.6V45z"></path>
+            )}
+          </svg>
+        </a>
+      </Link>
+      <svg
+        aria-label="Search and explore"
+        color="#262626"
+        fill="#262626"
+        height="24"
+        role="img"
+        viewBox="0 0 24 24"
+        width="24"
+      >
+        <path
+          d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z"
+          fill="none"
+          stroke="currentColor"
+          stroke-Linecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        ></path>
+        <line
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          x1="16.511"
+          x2="22"
+          y1="16.511"
+          y2="22"
+        ></line>
+      </svg>
+      <svg
+        aria-label="New post"
+        color="#262626"
+        fill="#262626"
+        height="24"
+        role="img"
+        viewBox="0 0 24 24"
+        width="24"
+      >
+        <path
+          d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        ></path>
+        <line
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          x1="6.545"
+          x2="17.455"
+          y1="12.001"
+          y2="12.001"
+        ></line>
+        <line
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          x1="12.003"
+          x2="12.003"
+          y1="6.545"
+          y2="17.455"
+        ></line>
+      </svg>
+      <svg
+        aria-label="Activity"
+        color="#262626"
+        fill="#262626"
+        height="24"
+        role="img"
+        viewBox="0 0 24 24"
+        width="24"
+      >
+        <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
+      </svg>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/my-suit-pic.jpg"
+          alt="profile pic"
+          className={styles.profilePic}
+          height={270}
+          width={270}
+          layout="responsive"
+        />
+      </div>
+    </div>
+  );
+}
