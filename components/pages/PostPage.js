d6c673bb86d40a488659ed79eece609860df577b
alshakerM <82781870+alshakerM@users.noreply.github.com>
2022-01-29 15:02:02 +0100

    Add mobile supports for expanded post

--- components/pages/PostPage.js
+++ components/pages/PostPage.js
@@ -2,11 +2,12 @@ import { Post } from '../Post/Post';
 import React from 'react';
 import { useSelect } from '@wordpress/data';
 
-export function PostPage({ postId, isFloating }) {
+export function PostPage({ postId, isFloating, onlyComments }) {
   const post = useSelect((select) => select('ig-posts').getPost(postId));
   return (
     <div className="content-section">
       <Post
+        onlyComments={onlyComments}
         datum={post}
         comments={post?.comments}
         isFloating={isFloating}
